0a35a6cf973387dff55ace242f699d05fd99ff55
Rory Flint <roryflintphoto@gmail.com>
2020-03-10 15:54:22 +0000

    Add color styles and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const StyledHeader = styled.h1`
+  font-size: 2rem;
+  font-family: 'Muli';
+    color: #E50914;
+`;
 
 function App() {
   return (
     <div className="App">
+      <StyledHeader><span>to</span>day</StyledHeader>
     </div>
   );
 }
---
+++ src/styles/theme.js
@@ -0,0 +1,9 @@
+export const theme = {
+  logoFont: 'Muli',
+  mainFont: 'Ubuntu',
+
+  lightBackground: '#FFFFFF',
+  darkText: '#202020',
+  lightText: '#616161',
+  logoText: '#E50914',
+};
