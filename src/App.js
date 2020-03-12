e89c256d9b380a048e31fdac16c1621c1933cdc4
Rory Flint <roryflintphoto@gmail.com>
2020-03-12 12:55:11 +0000

    Set up App for Imports + Import LandingPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+// Library Imports
 import React from 'react';
+import { Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+// Styling Imports
 import { Theme } from './styles/theme';
 import GlobalStyle from './styles/globalTheme';
+
+// Component Imports
+import LandingPage from './Views/LandingPage';
 
 function App() {
   return (
     <ThemeProvider theme={Theme}>
       <GlobalStyle />
-      <div className="App"></div>
+      <Route exact path='/' component={LandingPage} />
     </ThemeProvider>
   );
 }
---
+++ src/Views/LandingPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function LandingPage() {
+    return <h1>Hello!</h1>
+}
+
+export default LandingPage;
